db1d04ce461724c7dd316700142eef3173f866e8
Noah Olmos <nnolmos1@gmail.com>
2026-07-13 09:34:20 -0500

    Initial commit with root project files

---
+++ OliveIOS/OliveIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OliveIOSApp.swift
+//  OliveIOS
+//
+//  Created by SandboxLab on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct OliveIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
